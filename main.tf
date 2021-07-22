d8262e8a34929a5284e9b986ce5e001dee7bac8a
Roberth Strand <me@robstr.dev>
2021-07-22 10:53:42 +0200

    Initial commit, test workflow

--- main.tf
+++ main.tf
@@ -1,1 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.68.0"
+    }
+  }
 
+  backend "azurerm" {
+    resource_group_name  = "demo-github-terraform"
+    storage_account_name = "rsdemogithubterraform"
+    container_name       = "tfstate"
+    key                  = "production.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "test-rg"
+  location = "Norwegian East"
+}
