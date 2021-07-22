53698a1c95f3af623ef1c5a58ff7b5a774f2ee89
Roberth Strand <me@robstr.dev>
2021-07-22 15:54:37 +0200

    Diggi

--- main.tf
+++ main.tf
@@ -27,17 +27,17 @@ resource "azurerm_resource_group" "test" {
     source      = "Terraform"
   }
 }
-resource "azurerm_virtual_network" "cluster" {
+resource "azurerm_virtual_network" "test" {
   name                = "tfgh-cluster-vnet"
-  location            = azurerm_resource_group.cluster.location
-  resource_group_name = azurerm_resource_group.cluster.name
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "aks" {
   name                 = "aks"
-  resource_group_name  = azurerm_resource_group.cluster.name
-  virtual_network_name = azurerm_virtual_network.cluster.name
+  resource_group_name  = azurerm_resource_group.test.name
+  virtual_network_name = azurerm_virtual_network.test.name
   address_prefixes     = ["10.0.0.0/24"]
 }
 
