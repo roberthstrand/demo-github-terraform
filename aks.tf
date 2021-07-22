b888228fbd41982559c1b9d114dd13b43c66af58
Roberth Strand <me@robstr.dev>
2021-07-22 16:27:45 +0200

    Fix code

--- aks.tf
+++ aks.tf
@@ -7,15 +7,15 @@ module "kubernetes" {
   subnet_id      = azurerm_subnet.aks.id
   admin_groups   = data.azuread_groups.admins.object_ids
 
-  default_node_pool = [{
+  default_node_pool = {
     name                = "default"
     vm_size             = "Standard_B1s"
     node_count          = 1
     enable_auto_scaling = false
     min_count           = null
     max_count           = null
-    additional_settings = null
-  }]
+    additional_settings = {}
+  }
 
   depends_on = [azurerm_resource_group.test]
 }
